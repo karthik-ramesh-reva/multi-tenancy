aebaaf529beef330673b4ff1f5d91d4e8eaba755
karthik-ramesh-reva <karthik.ramesh@reva.ai>
2024-11-19 14:26:24 +0530

    Multi Tenancy

--- utils/customerConfig.ts
+++ utils/customerConfig.ts
@@ -23,7 +23,7 @@ export const customerConfigs: { [key: string]: CustomerConfig } = {
     mt1: {
         cognitoDomain: 'reva-auth-client36.auth.us-east-1.amazoncognito.com',
         clientId: '47j3nqkhp8dcktnaonr30mav2k',
-        clientSecret: '5lv29uq8rb21mo2scf2n768684rho5k8qal0sfc46ugp21gf58s',
+        clientSecret: '1rrur6n50etb5r57qtmsdtp6e1a9kiib5b1ops6c0uitoi8vhit4',
         userPoolId: 'us-east-1_8rnj3sLV2',
         region: 'us-east-1',
         redirectUri: 'https://mt1.ea.reva.ai/callback',
@@ -32,7 +32,7 @@ export const customerConfigs: { [key: string]: CustomerConfig } = {
     mt2: {
         cognitoDomain: 'reva-auth-test9.auth.us-east-1.amazoncognito.com',
         clientId: '7o20umpaan13mibjuuiod0407n',
-        clientSecret: '5lv29uq8rb21mo2scf2n768684rho5k8qal0sfc46ugp21gf58s',
+        clientSecret: 'rvim138u91hu7sqokvc036d91gnhjmr163g5pm7rkelu2mntf38',
         userPoolId: 'us-east-1_YEce83Zit',
         region: 'us-east-1',
         redirectUri: 'https://mt2.ea.reva.ai/callback',
@@ -41,7 +41,7 @@ export const customerConfigs: { [key: string]: CustomerConfig } = {
     mt3: {
         cognitoDomain: 'https://reva-auth-test6.auth.us-east-1.amazoncognito.com',
         clientId: '476f4hlm7kfm60ic8smehrp7vl',
-        clientSecret: '5lv29uq8rb21mo2scf2n768684rho5k8qal0sfc46ugp21gf58s',
+        clientSecret: '68p97cturscub2a7mhgutlt8b8d7fv8q5rg4elde9utals54ise',
         userPoolId: 'us-east-1_B7VSw0fEc',
         region: 'us-east-1',
         redirectUri: 'https://mt3.ea.reva.ai/callback',
@@ -50,7 +50,7 @@ export const customerConfigs: { [key: string]: CustomerConfig } = {
     mt4: {
         cognitoDomain: 'reva-auth-test5.auth.us-east-1.amazoncognito.com',
         clientId: 'd98usmnlf9979tci79on5g8nj',
-        clientSecret: '5lv29uq8rb21mo2scf2n768684rho5k8qal0sfc46ugp21gf58s',
+        clientSecret: 'fophmnveiidvksj6g11ccl9llcgc2jkkrrpcrnh222cf8gc1eke',
         userPoolId: 'us-east-1_Yojn9QwZS',
         region: 'us-east-1',
         redirectUri: 'https://mt4.ea.reva.ai/callback',
